5d4bec7b59e30fd82d4344b2c64e25bd46d5cfb1
kissbill <milerik@hotmail.com>
2019-07-21 17:56:08 +0200

    updating with tabs and checkbuttons with names

--- GUI/Main.py
+++ GUI/Main.py
@@ -20,26 +20,34 @@ help_menu.add_command(label='About')
 tab_control = ttk.Notebook(window)
 tab1 = ttk.Frame(tab_control)
 tab2 = ttk.Frame(tab_control)
-tab_control.add(tab1, text='First')
-tab_control.add(tab2, text='Second')
-lbl1 = Label(tab1, text='label1')
-lbl1.grid(column=0, row=0)
-lbl2 = Label(tab2, text='label2')
-lbl2.grid(column=0, row=0)
+tab_control.add(tab1, text='VERIZON')
+tab_control.add(tab2, text='EPAM')
+# Spacing
+lbl1 = Label(tab1, text='label1', padx=5, pady=5)
+lbl1.grid(column=3, row=0)
+# RadioButtons
+# v = IntVar()
+# Radiobutton(tab1, text='GfG', variable=v, value=1).pack(anchor=W)
+# Radiobutton(tab1, text='MIT', variable=v, value=1).pack(anchor=W)
+var1 = IntVar()
+Checkbutton(tab1, text='TestAutomate', variable=var1).grid(row=0, sticky=W)
+var2 = IntVar()
+Checkbutton(tab1, text='Email', variable=var2).grid(row=1, sticky=W)
+
+# lbl2 = Label(tab2, text='label2')
+# lbl2.grid(column=0, row=0)
+var1 = IntVar()
+Checkbutton(tab2, text='ToDo', variable=var1).grid(row=0, sticky=W)
+var2 = IntVar()
+Checkbutton(tab2, text='NeedToCheck', variable=var2).grid(row=1, sticky=W)
 tab_control.pack(expand=1, fill='both')
 
-# Tags
-v = IntVar()
-Radiobutton(window, text='GfG', variable=v, value=1).pack(anchor=W)
-Radiobutton(window, text='MIT', variable=v, value=1).pack(anchor=W)
+
 
 # Input
 T = Text(window, height=2, width=30)
 T.pack()
 
-# var1 = IntVar()
-# Checkbutton(window, text='male', variable=var1).grid(row=0, sticky=W)
-# var2 = IntVar()
-# Checkbutton(window, text='female', variable=var2).grid(row=1, sticky=W)
+
 
 window.mainloop()
